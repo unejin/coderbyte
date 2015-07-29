99ba18d9603a9f7e11f8aa92dad92343aaf32086
unejin <unejini@gmail.com>
2015-07-29 15:23:58 +0900

    Create ABCheck.rb

---
+++ ABCheck.rb
@@ -0,0 +1,10 @@
+def ABCheck(str)
+
+  # code goes here
+  return str.match(/a...b/) != nil
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ABCheck(STDIN.gets)           
